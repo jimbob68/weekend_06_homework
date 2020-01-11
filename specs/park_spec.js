953559ffd967e376f953407ec2566b5b34dd6dba
James Fraser <jimbob4t@virginmedia.com>
2020-01-11 20:34:47 +0000

    should have ticket price passing

--- specs/park_spec.js
+++ specs/park_spec.js
@@ -15,7 +15,11 @@ describe('Park', function() {
     assert.strictEqual(actual, 'Jurassic Park');
   });
 
-  it('should have a ticket price');
+  it('should have a ticket price', function (){
+    const actual = park.ticketPrice;
+    assert.strictEqual(actual, 100);
+  });
+
 
   it('should have a collection of dinosaurs');
 
